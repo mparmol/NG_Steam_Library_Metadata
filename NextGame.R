14dc5d1a90becd20165d76f78e473f73d5197e41
Marcos Parras <mparmol@gmail.com>
2022-11-29 17:32:47 +0100

    updated

--- NextGame.R
+++ NextGame.R
@@ -6,8 +6,30 @@ https://github.com/Depressurizer/Depressurizer/releases
 
 ### Extract gameplay time
 
-system("node New.js > aux_time.txt")
+game_list<-read.delim("Games.txt")
 
-data_time<-read.delim("aux_time.txt")
+for(i in 1:dim(game_list)[1])
+{
+  pasted_value=paste(strsplit(game_list[i,],split = " ")[[1]],collapse =  "_")
+  
+  system(paste("node New.js ",pasted_value," > aux_time.txt", sep=""))
+  
+  data_time<-read.delim("aux_time.txt")
+  
+  if(dim(data_time)[1]>0)
+  {
+    if(gsub(",","",strsplit(grep("gameplayMain:",data_time[,1],value=T), "Main: ")[[1]][2])>0)
+    {
+      print(paste(game_list[i,],": ",gsub(",","",strsplit(grep("gameplayMain:",data_time[,1],value=T), "Main: ")[[1]][2]),"h",sep = ""))
+      
+    }else
+    {
+      print(paste(game_list[i,],": ","Sin registro de tiempo",sep = ""))
+      
+    }
+  }else
+  {
+    print(paste(game_list[i,],": NA",sep = "")) 
+  }
+}
 
-gsub(",","",strsplit(grep("gameplayMain:",data_time[,1],value=T), "Main: ")[[1]][2])
