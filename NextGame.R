b973905530477e0b596b9dd1d57693b3aeea8c1d
Marcos Parras <mparmol@gmail.com>
2022-11-29 12:07:39 +0100

    new ver

--- NextGame.R
+++ NextGame.R
@@ -1,4 +1,13 @@
 ##NextGame
 
+### Extract game list, from windows
+
+https://github.com/Depressurizer/Depressurizer/releases
+
+### Extract gameplay time
+
 system("node New.js > aux_time.txt")
-as.vector(system("grep 'gameplayMain:' aux_time.txt"))
+
+data_time<-read.delim("aux_time.txt")
+
+gsub(",","",strsplit(grep("gameplayMain:",data_time[,1],value=T), "Main: ")[[1]][2])
