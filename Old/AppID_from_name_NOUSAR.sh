686c5b7fea17ca7cb0b2e431ea8afa6fbdfe41c0
mparmol <mparmol@gmail.com>
2023-01-25 23:51:46 +0100

    updt

--- Old/AppID_from_name_NOUSAR.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash    
-#Example Usage:  ./playercount.sh "Left 4 Dead 2"    
-appid=$(curl -s https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq ".applist.apps[] | select(.name==\"$1\")" | jq '.appid')    
-#curl -s https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid="$appid" | jq '.response.player_count'
-echo $appid
-
-steamspy.com/api.php?request=appdetails&appid=$appid
